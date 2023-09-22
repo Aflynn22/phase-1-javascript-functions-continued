70dd121b804788c83ca7967ae6f7deef9ee6add4
Austin <Austin.flynn1992@gmail.com>
2023-09-21 20:22:08 -0500

    phase-1-javascript-functions-continued$

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+
+function mondayWork(work = 'go to the office'){
+    return (`This Monday, I will ${work}.`)
+}
+
+
+
+function wrapAdjective(wrapper="*"){
+    return function(STR){
+    return `You are ${wrapper}${STR}${wrapper}!`
+     }
+}
+console.log(emphatic)
